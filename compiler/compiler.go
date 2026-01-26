d61a543d437a074a109067bd1d009d4eccbd6a3b
rmuell9 <rmatthewmueller@gmail.com>
2026-01-26 16:32:55 -0600

    progress

---
+++ compiler/compiler.go
@@ -0,0 +1,35 @@
+package compiler
+
+import (
+	"my-compiler/interpreter/ast"
+	"my-compiler/code"
+	"my-compiler/interpreter/object"
+)
+
+type Compiler struct {
+	instuctions code.Instructions
+	constants []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants: []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	return nil
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants: c.constants,
+	}
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants []object.Object
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module compiler
+module my-compiler
 
 go 1.25.6
